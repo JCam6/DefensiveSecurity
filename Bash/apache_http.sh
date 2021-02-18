42168008547b9de3fb78ba70ff2c5b3b895cf2bc
JCam6 <45147733+JCam6@users.noreply.github.com>
2021-02-18 11:03:06 -0500

    Add files via upload

---
+++ Bash/apache_http.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# make this file executable with chmod +x before running
+
+yum install httpd httpd-manual nano -y
+touch /var/www/html/page.html
+echo "Access Denied. Contact Administrators." >> /var/www/html/page.html
+cd /etc/httpd/conf.d/
+wget https://raw.githubusercontent.com/JCam6/DefensiveSecurity/main/Bash/site1.conf
+systemctl enable httpd.service
+systemctl start httpd.service
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-port=80/tcp
+exit
+
+# open web browser at http://<machineIPaddress>/page.html
+# update web page script at /var/www/html/page.html
+# update config file at /etc/httpd/conf.d/site1.conf
